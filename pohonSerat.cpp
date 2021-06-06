744ce2a6793ed35c426f23d997e5a93a5c8594f3
bagus2705 <bagusprasetyo2705@gmail.com>
2021-06-06 23:44:08 +0700

    test

--- pohonSerat.cpp
+++ pohonSerat.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 
 struct Node{
+ std::string status;
+ std::string data;
  float value;
    Node *left;
    Node *right;
@@ -8,9 +10,13 @@ struct Node{
 
 using pointer=Node*;
 using Tree=pointer;
+void createRoot(pointer &newNode,float value){
 
-void createNode(pointer& newNode, float value){
+}
+void createNode(pointer& newNode, std::string status,std::string data,float value){
    newNode = new Node;
+   newNode->status = status;
+   newNode->data = data;
    newNode->value = value;
    newNode->left = nullptr;
    newNode->right = nullptr;
@@ -33,4 +39,11 @@ void insertBST(Tree& root, pointer newNode){
 int main(){
  Tree pohon=nullptr;
  pointer newNode;
- float BB, TB, BMI;
+ int usia;
+ void createRoot(pointer &newNode,float value);insertBST(pohon,newNode);
+  createNode(newNode,"rendah","120.0/3.0",3); insertBST(pohon,newNode);
+  createNode(newNode,"sedang","240.0/10.0",7); insertBST(pohon,newNode);
+
+
+  std::cout << "Masukkan Data Diri Anda" << std::endl;
+ std::cout << "Usia"; std::cin >>usia;
