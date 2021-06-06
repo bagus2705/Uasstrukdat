2ee0b50cf3f4f101a295d320562256c499950765
ajsbas <abraham20003@mail.unpad.ac.id>
2021-06-06 22:48:20 +0700

    insert tree

--- pohonKarbo.cpp
+++ pohonKarbo.cpp
@@ -9,3 +9,16 @@ tree Root;
 tree createTree() { 
   return nullptr; 
   }
+
+void insertTree(tree root,pointer newNode){
+  if(root == nullptr){
+    root = newNode;
+    }else{
+  bool isLowerThanRoot = (newNode->urutan < root->urutan);
+
+  if(isLowerThanRoot){
+    insertTree(root->left, newNode);}
+  else{
+    insertTree(root->right, newNode);
+}
+}
