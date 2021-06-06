12cf00ba6214b8273c2f8daa5742d8d7c0e11e0c
bagus2705 <bagusprasetyo2705@gmail.com>
2021-06-06 21:11:36 +0700

    contoh

--- pohonSerat.cpp
+++ pohonSerat.cpp
@@ -1,25 +1,39 @@
+#include <iostream>
+
 struct Node{
-    int data;
+    int urutan;
+    std::string data;
     Node *left;
     Node *right;
 };
 using pointer=Node*;
 using tree=pointer;
-tree Root;
+tree root;
 
 tree createTree() { 
   return nullptr; 
   }
-pointer createElement(int data) {
+pointer createElement(std::string data,int urutan) {
  pointer elementBaru=new Node;
  elementBaru=new Node;
+ elementBaru->urutan=urutan;
  elementBaru->data=data;
  elementBaru->left=nullptr;
  elementBaru->right=nullptr;
  return elementBaru;
 }
-void insertTree(pointer newElement,tree Root){
+void insertTree(tree root,pointer newNode){
+  if(root == nullptr){
+    root = newNode;
+    }else{
+  bool isLowerThanRoot = (newNode->urutan < root->urutan);
 
+  if(isLowerThanRoot){
+    insertTree(root->left, newNode);}
+  else{
+    insertTree(root->right, newNode);
+}
+}
 }
 
 void cekUmur(int umur,tree Root){
